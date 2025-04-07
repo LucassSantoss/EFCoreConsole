e1b5cf267401f6dd996b7d5a6cb7c8b4d4b8f573
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-04-07 20:13:50 -0300

    feat: add the first items to database

--- EFCoreConsole/Program.cs
+++ EFCoreConsole/Program.cs
@@ -1,7 +1,19 @@
 ﻿using EFCoreConsole.DbContexts;
+using EFCoreConsole.Entities;
 using Microsoft.EntityFrameworkCore;
 
+// Apenas cria o banco de dados se não estiver criado
 using (var context = new Context())
 { 
     context.Database.EnsureCreated();
+}
+
+using var db = new Context();
+// db.Add(new Filme {Titulo="Homem de ferro", Ano=2010});
+// db.SaveChanges();
+
+var filmes = db.Filmes.ToList();
+foreach (var filme in filmes)
+{
+    Console.WriteLine(filme.Titulo);
 }
