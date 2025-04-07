f8016d3e46e2326c62fdac56061f98850ace84ca
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-04-07 00:07:38 -0300

    feat: create dbContext to create db tables

--- EFCoreConsole/DbContexts/Context.cs
+++ EFCoreConsole/DbContexts/Context.cs
@@ -1,6 +1,15 @@
-namespace EFCoreConsole.DbContext;
+using EFCoreConsole.Entities;
+using Microsoft.EntityFrameworkCore;
 
-public class Context : Microsoft.EntityFrameworkCore.DbContext
+namespace EFCoreConsole.DbContexts;
+
+public class Context : DbContext
 {
-    
+    // Gera a tabela Filmes com base na classe filme
+    public DbSet<Filme> Filmes { get; set; }
+    // Gera a tabela Diretores com base na classe Diretor
+    public DbSet<Diretor> Diretores { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=EFCoreConsole");
 }
--- EFCoreConsole/obj/Debug/net9.0/EFCoreConsole.AssemblyInfo.cs
+++ EFCoreConsole/obj/Debug/net9.0/EFCoreConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCoreConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5570081137b3a80bfb543f24c6e88d7d3da75e4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCoreConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCoreConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
