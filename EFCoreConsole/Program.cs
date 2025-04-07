04b48425d92e1d11c1e66803f8fa071a81edc428
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-04-07 20:27:12 -0300

    feat: remove an entity from database

--- EFCoreConsole/Program.cs
+++ EFCoreConsole/Program.cs
@@ -10,19 +10,16 @@ using (var context = new Context())
 
 using var db = new Context();
 
-// Edita um dado do banco de dados
 var filme = db.Filmes.Find(1);
 if (filme != null)
 {
-    filme.Titulo = "Home de ferro 1";
-    filme.Diretor = new Diretor() { Nome = "Lucas" };
+    db.Remove(filme);
+    db.SaveChanges();
 }
-db.SaveChanges();
 
 // Lista todos os filmes
 var filmes = db.Filmes.ToList();
 foreach (var item in filmes)
 {
-    if (item != null)
-        Console.WriteLine($"{item.Id} {item.Titulo}");
+    Console.WriteLine($"{item.Id} {item.Titulo}");
 }
