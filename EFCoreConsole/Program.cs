6079d23573975ba1b3d6a6cb3203e2d3827456bc
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-04-07 20:40:22 -0300

    refactor: refact program.cs - ensure create database

--- EFCoreConsole/Program.cs
+++ EFCoreConsole/Program.cs
@@ -2,20 +2,12 @@
 using EFCoreConsole.Entities;
 using Microsoft.EntityFrameworkCore;
 
-// Apenas cria o banco de dados se não estiver criado
-using (var context = new Context())
-{ 
-    context.Database.EnsureCreated();
-}
-
 using var db = new Context();
 
+// Apenas cria o banco de dados se não estiver criado
+db.Database.EnsureCreated();
+
 var filme = db.Filmes.Find(1);
-if (filme != null)
-{
-    db.Remove(filme);
-    db.SaveChanges();
-}
 
 // Lista todos os filmes
 var filmes = db.Filmes.ToList();
